ddc54f1b6167a015c19d4c924ae38097edfe8ad5
Tair Assimov <tair@assimov.net>
2014-01-22 11:48:04 +0100

    Add nested Contact route and show it

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ App = Ember.Application.create();
 
 // Ember Router maps URLs to application resources
 App.Router.map(function() {
-  this.resource("contacts");
+  this.resource("contacts", function() {
+    this.resource("contact", { path: ":contact_id" });
+  });
 });
 
 // Embers Routes responsible for hooking models
